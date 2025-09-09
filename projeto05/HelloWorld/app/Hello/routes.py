fc5c2df142a7559c78a0431b706c40b3b797901d
thaiseinsfelddarocha <t.da-rocha@cf-partners>
2025-09-08 21:41:39 -0300

    feat(01):criando crud

--- projeto05/HelloWorld/app/Hello/routes.py
+++ projeto05/HelloWorld/app/Hello/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 from ..models import User, db
 
 hello_bp = Blueprint('hello', __name__)
@@ -18,3 +18,29 @@ def about():
 
 def test():
     return "Nova rota teste"
+
+@hello_bp.route('/novoUsuario', methods=["POST"]) 
+def novoUsuario():
+    nome_usuario = request.form['nome_usuario']
+
+    novo_usuario = User(username=nome_usuario)
+    db.session.add(novo_usuario)
+    db.session.commit()
+    return redirect('/')
+
+
+@hello_bp.route('/removerUsuario/<int:usuario_id>', methods=["POST"]) 
+def removerUsuario(usuario_id):
+    usuario = User.query.get(usuario_id)
+    if usuario:
+        db.session.delete(usuario)
+        db.session.commit()
+    return redirect(url_for('hello.index'))
+
+@hello_bp.route('/editarUsuario/<int:usuario_id>', methods=["POST"])
+def editarUsuario(usuario_id):
+    usuario = User.query.get(usuario_id)
+    if usuario:
+        usuario.username = request.form['nome_usuario'] 
+        db.session.commit()
+    return redirect(url_for('hello.index'))
